e259c86cf5948d28bdf6fc553b157fcd8ff437e7
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-08 15:22:53 +0700

    Add project files.

---
+++ SampleAPI/Controllers/HelloController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Models;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        List<Student> lstStudent;
+        public HelloController()
+        {
+            lstStudent = new List<Student>
+            {
+                new Student{StudentID=1,FirstName="Erick",LastName="Kurniawan"},
+                new Student{StudentID=2,FirstName="Budi",LastName="Anduk" },
+                new Student{StudentID=3,FirstName="Joko",LastName="Purwadi"}
+            };
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return lstStudent;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Student student)
+        {
+            lstStudent.Add(student);
+            return StatusCode(201, student);
+        } 
+
+        [HttpGet]
+        [Route("{id}")]
+        public Student Get(int id)
+        {
+            var result = lstStudent.Where(s => s.StudentID == id).FirstOrDefault();
+            return result;
+        }
+
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Hello World";
+        }
+    }
+}
